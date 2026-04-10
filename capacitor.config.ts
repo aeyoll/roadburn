499fc9a88f61ca80dc93a3e098ece2c518910651
Jean-Philippe Bidegain <jp@bidega.in>
2026-04-10 22:40:43 +0200

    chore: update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'in.bidega.roadburn',
-  appName: 'roadburn',
+  appName: 'Roadburn',
   webDir: 'dist'
 };
 
