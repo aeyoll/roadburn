090a0879e5d056b68efc23fc47bd90084d601536
Jean-Philippe Bidegain <jp@bidega.in>
2026-04-10 13:36:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'roadburn',
+  webDir: 'dist'
+};
+
+export default config;
